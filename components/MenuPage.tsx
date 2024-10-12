e5780c5b00976784ff1eb655a9c9488e458407aa
blupxd <matija.stefanovic050@gmail.com>
2024-10-12 15:14:28 +0200

    xd

--- components/MenuPage.tsx
+++ components/MenuPage.tsx
@@ -38,7 +38,7 @@ const MenuPage = () => {
         </GradientLink>
       </div>
 
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 z-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 z-10">
         <MenuCards />
         <Card className="h-52 bg-gradient-to-tr from-purple-800/10 to-cyan-800/10">
           <CardHeader>
